9b50b1367137d425e3b24a103015b73e606e061e
brian.lavallee@invite-comm.jp <Brian LaVallee>
2019-02-20 08:11:26 +0000

    Working on testing and rebuild

--- src/Models/ContactMedium.php
+++ src/Models/ContactMedium.php
@@ -3,7 +3,7 @@
 namespace Poing\Ylem\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Poing\Ylem\Models\Medium as Medium;
+use Poing\Ylem\Models\Medium;
 
 class ContactMedium extends Model
 {
@@ -14,7 +14,7 @@ class ContactMedium extends Model
 
     protected $table = 'contact_medium';
 
-    public function party() {
+    public function contact() {
         return $this->morphTo();
     }
 
@@ -25,7 +25,7 @@ class ContactMedium extends Model
     /**
      * Used to test phpunit access to this class
      */
-    public static function test()
+    public static function probe()
     {
         return true;
     }
--- src/Models/PartyRelationship.php
+++ src/Models/PartyRelationship.php
@@ -11,27 +11,31 @@ class PartyRelationship extends Node
 
     // Soft deletes not recommended with Baum
     use SoftDeletes;
-	protected $with = ['party', 'children'];
-    protected $dates = ['deleted_at'];
+
+	protected $with = [
+	    'party',
+	    'children'
+	];
+
+    protected $dates = [
+        'deleted_at'
+    ];
 
     public function party() {
         return $this->morphTo();
     }
-
+/*
     function delete()
     {
         // May not be desired, using soft delete for party
-        //$this->party()->existsDuring()->delete();
-        //$org = $this->party();
-        //$org->existsDuring()->delete();
         $this->party()->delete();
         parent::delete();
     }
-
+*/
     /**
      * Used to test phpunit access to this class
      */
-    public static function test()
+    public static function probe()
     {
         return true;
     }
