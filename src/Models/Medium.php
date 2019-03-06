fba38ed6994c9acb6a035a0136f58bab8c338f18
brian.lavallee@invite-comm.jp <Brian LaVallee>
2019-03-06 04:16:11 +0000

    recent rebuild changes

---
+++ src/Models/Medium.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Poing\Ylem\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Poing\Ylem\Models\ContactMedium as ContactMedium;
+
+class Medium extends Model
+{
+    protected $table = 'medium';
+
+    public function contactMedium()
+    {
+        return $this->belongsTo(ContactMedium::class);
+    }
+
+    /**
+     * Used to test phpunit access to this class
+     */
+    public static function probe()
+    {
+        return true;
+    }
+}
